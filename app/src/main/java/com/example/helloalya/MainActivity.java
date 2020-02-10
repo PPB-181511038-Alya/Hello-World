d3d754f609a2850689caa9616e5c9b60ef9de31b
alya maharani <alya.maharani.tif18@polban.ac.id>
2020-02-10 20:11:42 +0700

    hello world

---
+++ app/src/main/java/com/example/helloalya/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloalya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
